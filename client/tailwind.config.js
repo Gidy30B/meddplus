8a0f4caf0a46231784f97ae23bbc887a9b566ee8
Gidy30B <154205415+Gidy30B@users.noreply.github.com>
2024-05-04 23:42:08 +0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Enable dark mode variant
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
       fontFamily: {
         Inter: ['Inter', 'sans-serif'],
         ReditMono: ['ReditMono', 'monospace'],
-      }
+      },
+      colors: {
+        // Example dark theme colors
+        dark: {
+          primary: '#1a202c', // Dark background color
+          secondary: '#2d3748', // Dark secondary background color
+          text: '#ffffff', // Text color
+          accent: '#f6e05e', // Accent color
+          // Add more colors as needed
+        },
+      },
     },
   },
+
   plugins: [],
 }
