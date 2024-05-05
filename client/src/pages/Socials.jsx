8caf500bfd6e05a5ec157e9886b041b0f8fd29c1
Gidy30B <154205415+Gidy30B@users.noreply.github.com>
2024-05-05 13:29:26 +0300

    Update Socials.jsx

--- client/src/pages/Socials.jsx
+++ client/src/pages/Socials.jsx
@@ -155,7 +155,7 @@ const Socials = () => {
 
   return (
     <>
-      <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-slate-800 lg:rounded-lg h-screen overflow-hidden">
+      <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-slate-950 lg:rounded-lg h-screen overflow-hidden">
         <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
           {/* LEFT */}
           <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto">
