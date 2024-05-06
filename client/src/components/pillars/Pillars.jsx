4c882d097cd3b22c539f669a161a38fd2729e1c9
Gidy30B <154205415+Gidy30B@users.noreply.github.com>
2024-05-06 15:26:25 +0300

    Update Pillars.jsx

--- client/src/components/pillars/Pillars.jsx
+++ client/src/components/pillars/Pillars.jsx
@@ -127,11 +127,11 @@ const DoctorsSectionWithAnimation = () => {
 
 const DoctorsSection = () => {
   const doctors = [
-    { name: "Doctor 1", image: "doctor1.jpg", description: "Description of Doctor 1" },
-    { name: "Doctor 2", image: "doctor1.jpg", description: "Description of Doctor 2" },
-    { name: "Doctor 3", image: "doctor1.jpg", description: "Description of Doctor 3" },
-    { name: "Doctor 4", image: "doctor1.jpg", description: "Description of Doctor 4" },
-    { name: "Doctor 5", image: "doctor1.jpg", description: "Description of Doctor 5" }
+    { name: "Doctor 1", image: "doctor1", description: "Description of Doctor 1" },
+    { name: "Doctor 2", image: "doctor1", description: "Description of Doctor 2" },
+    { name: "Doctor 3", image: "doctor1", description: "Description of Doctor 3" },
+    { name: "Doctor 4", image: "doctor1", description: "Description of Doctor 4" },
+    { name: "Doctor 5", image: "doctor1", description: "Description of Doctor 5" }
   ];
 
   return (
